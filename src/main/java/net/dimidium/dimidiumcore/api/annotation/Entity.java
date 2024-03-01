51081773fd8939832d58080765b05398a2adc1f8
Dimidium <dimidium@dimidiummods.com>
2024-03-01 11:55:14 +0000

    Add some annotations, functionality to come later

---
+++ src/main/java/net/dimidium/dimidiumcore/api/annotation/Entity.java
@@ -0,0 +1,10 @@
+package net.dimidium.dimidiumcore.api.annotation;
+
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
+@Target(ElementType.TYPE)
+public @interface Entity
+{
+
+}
